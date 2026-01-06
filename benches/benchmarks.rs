99a29d868a9abd0c7dad4517eaf275e82c1d8dc8
Mathias Lüer <mathias.lueer@etailer.de>
2026-01-06 19:00:27 +0100

    refactor: remove commented-out lump indexing benchmark and simplify audio conversion code

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -22,28 +22,6 @@ fn bench_wad_from_bytes(b: &mut Criterion) {
     group.finish();
 }
 
-// fn bench_indexing_lumps(b: &mut Criterion) {
-//     let tokens = vec![
-//         LumpToken::MarkerStart("S_START".to_string()),
-//         LumpToken::Lump("TEST1".to_string(), LumpRef::new(0, 0, 0)),
-//         LumpToken::Lump("TEST2".to_string(), LumpRef::new(0, 0, 0)),
-//         LumpToken::MarkerEnd("S_END".to_string()),
-//         LumpToken::Lump("TEST3".to_string(), LumpRef::new(0, 0, 0)),
-//     ];
-//
-//     let mut group = b.benchmark_group("Wad indexing lumps");
-//     group.throughput(Throughput::Elements(tokens.len() as u64));
-//     group.sample_size(100);
-//
-//     group.bench_function("index_lumps", |b| {
-//         b.iter(|| {
-//             index_tokens(&tokens).unwrap();
-//         })
-//     });
-//
-//     group.finish();
-// }
-
 fn bench_converting_sprites(b: &mut Criterion) {
     let wad_data = WAD_DATA;
     let wad = WadIndex::from_bytes("freedoom2.wad".to_string(), &wad_data).unwrap();
@@ -108,8 +86,7 @@ fn bench_converting_sprites(b: &mut Criterion) {
 }
 
 fn bench_converting_audio(b: &mut Criterion) {
-    let wad_data = WAD_DATA;
-    let wad = WadIndex::from_bytes("freedoom1.wad".to_string(), &wad_data).unwrap();
+    let wad = WadIndex::from_bytes("freedoom1.wad".to_string(), WAD_DATA).unwrap();
     let filtered_lumps: Vec<&LumpRef> = wad
         .get_lump_index()
         .iter()
@@ -127,7 +104,7 @@ fn bench_converting_audio(b: &mut Criterion) {
         b.iter(|| {
             for lump_ref in &filtered_lumps {
                 let data = lump_ref.data();
-                let _sample = wad_rs::audio::SoundSample::try_from(data.as_ref()).unwrap();
+                let _ = wad_rs::audio::SoundSample::try_from(data).unwrap();
             }
         })
     });
@@ -138,7 +115,6 @@ fn bench_converting_audio(b: &mut Criterion) {
 criterion_group!(
     benches,
     bench_wad_from_bytes,
-    // bench_indexing_lumps,
     bench_converting_sprites,
     bench_converting_audio,
 );
